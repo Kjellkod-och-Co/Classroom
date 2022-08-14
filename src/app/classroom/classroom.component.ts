796632642bbdf5f605db2c610ada4b9b180eb0c5
Max Carlquist <tenkaklet@gmail.com>
2022-08-14 16:03:48 +0200

    Begun user presence

--- src/app/classroom/classroom.component.ts
+++ src/app/classroom/classroom.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,19 +9,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./classroom.component.scss']
 })
 export class ClassroomComponent implements OnInit {
-  constructor(public fAuth: AngularFireAuth, private router: Router) { }
+  constructor(public fAuth: AngularFireAuth, private router: Router, private db: AngularFireDatabase) { }
   photoUrl: any;
+  users: any;
   ngOnInit(): void {
     // get the user.
+    const userRef = this.db.object('online-users');
     this.fAuth.user.subscribe(u => {
+      userRef.update({user: u?.displayName});
       this.photoUrl = u?.photoURL;
     });
-    
+    this.getOnlineUsers();
   }
 
   logout() {
     this.fAuth.signOut();
     this.router.navigate(['auth']);
+  }
+
+  getOnlineUsers() {
+    this.db.object('online-users').valueChanges().subscribe((users: any) => {
+      const onlineUsers = Object.keys(users).map((key) => users[key]);
+      this.users = onlineUsers;
+    });
+    
   }
 
 }
